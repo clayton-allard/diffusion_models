e4de6b43e699bb90f43d7be9bfc6d156a268e2aa
Clayton Allard <clayton.allard@stat.ubc.ca>
2024-04-13 21:20:03 -0700

    Good progress but no clue if it actually works

--- code/models.py
+++ code/models.py
@@ -10,24 +10,49 @@ class Unet(nn.Module):
         self.device = device
         self.emb_dim = emb_dim
 
+        self.time_embedding = nn.Linear(1, emb_dim, device=device)
+
         self.down = nn.ModuleList([Block(channels, emb_dim)] + \
                                   [Block(emb_dim * 2 ** i, emb_dim * 2 ** (i + 1)) for i in range(layers)])
 
-        self.up = nn.ModuleList([layer for i in reversed(range(layers)) for layer in [
-                                 nn.ConvTranspose2d(emb_dim * 2 ** i, emb_dim * 2 ** (i + 1), 3, padding=1),
-                                 Block(emb_dim * 2 ** (i + 1), emb_dim * 2 ** (i + 2))
-                          ]])
+        self.up = nn.ModuleList([Block(emb_dim * 2 ** (i + 1), emb_dim * 2 ** i) for i in reversed(range(layers))] + \
+                                [Block(emb_dim, channels)])
 
-    def forward(self):
-        raise NotImplementedError()
+        self.trans_conv = nn.ModuleList([nn.ConvTranspose2d(emb_dim * 2 ** (i + 1),
+                                                            emb_dim * 2 ** i,
+                                                            kernel_size=2,
+                                                            stride=2) for i in reversed(range(layers))] + \
+                                [Block(emb_dim, channels)])
+
+        self.center = Block(emb_dim * 2 ** layers, emb_dim * 2 ** layers)
+
+    def forward(self, x, t):
+        skip_connections = []
+        t = t.unsqueeze(-1).type(torch.float)
+
+        for down in self.down:
+            x = down(x) + self.time_embedding(t)
+            skip_connections += [x]
+            x = nn.MaxPool2d(kernel_size=2)(x)
+
+        skip_connections.reverse()
+
+        x = self.center(x)
+
+        for up, skip, trans in zip(self.up, skip_connections, self.trans_conv):
+            x = trans(x)
+            x = torch.cat(x, skip)
+            x = up(x) + self.time_embedding(t)
+
 
 
 class Block(nn.Module):
     def __init__(self, inp_channels, out_channels, kernel_size=3, padding=1, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.conv = nn.Sequential(nn.BatchNorm2d(inp_channels),
+        self.conv = nn.Sequential(nn.Conv2d(inp_channels, out_channels, kernel_size, padding=padding, device=self.device),
+                                  nn.BatchNorm2d(out_channels),
                                   nn.ReLU(inplace=True),
-                                  nn.Conv2d(out_channels, out_channels, kernel_size, padding=padding),
+                                  nn.Conv2d(out_channels, out_channels, kernel_size, padding=padding, device=self.device),
                                   nn.BatchNorm2d(out_channels),
                                   nn.ReLU(inplace=True))
 
